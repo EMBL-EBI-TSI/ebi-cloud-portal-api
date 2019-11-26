94290cfac6a30fbd1dc0d0a29d56dbd13b89ce03
Felix Xavier Amaladoss <famaladoss@ebi.ac.uk>
2019-11-26 16:16:05 +0000

    Add more Test cases to test restrict deployment scenarios
    Add more integration tests to cover negative cases.

--- src/integration-test/java/uk/ac/ebi/tsi/portal/api/deployment/DeploymentRestControllerIT.java
+++ src/integration-test/java/uk/ac/ebi/tsi/portal/api/deployment/DeploymentRestControllerIT.java
@@ -1,10 +1,13 @@
 package uk.ac.ebi.tsi.portal.api.deployment;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.apache.log4j.Logger;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,14 +16,18 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import uk.ac.ebi.tsc.portal.BePortalApiApplication;
+import uk.ac.ebi.tsc.portal.api.deployment.service.ConfigurationNotUsableForApplicationException;
 import uk.ac.ebi.tsc.portal.config.WebConfiguration;
+import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -38,27 +45,29 @@ public class DeploymentRestControllerIT {
 	private MockMvc mockMvc;
 
 	@Value("${aapUserName}") 
-	private String aapUserName;
+	private String testUserName;
 	
 	@Value("${aapPassword}")
-	private String aapPassword;
+	private String testPassword;
+
+	private String TEST_USER_REFERENCE = "usr-d8749acf-6a22-4438-accc-cc8d1877ba36";
+
+	private String AJAY_USER_REFERENCE = "usr-9832620d-ec53-43a1-873d-efdc50d34ad1";
+
+	@Value("${ajayUserName}")
+	private String ajayUserName;
+
+	@Value("${ajayPassword}")
+	private String ajayPassword;
 	
 	@Value("${aapUrl}")
 	private String aapUrl;
 
-	private String token;
-
-	@Before
-	public void setup() throws Exception{
-		//get jwt token
-		ResponseEntity<String> response = restTemplate.withBasicAuth(aapUserName, aapPassword)
-				.getForEntity(aapUrl, String.class);
-		token = response.getBody();
-	}
-
 
 	@Test
-	public void testIfApplicationsHavingDeploymentsAreDeleted() throws Exception{ 
+	public void canCreateConfig() throws Exception{
+
+		String token = getToken(ajayUserName, ajayPassword);
 
 		//add cloud provider
 		String cloudProviderJson = "{\n  \"name\" : \"Embassy OpenStack\",\n  \"cloudProvider\" : \"OSTACK\",\n "
@@ -91,7 +100,7 @@ public class DeploymentRestControllerIT {
 				.andExpect(jsonPath("name").value("Pass"))
 				.andReturn();
 
-		/*add configuration
+		//add configuration
 		String configurationJson = "{\n\"name\":\"Pass\",\n\"cloudProviderParametersName\":\"Embassy OpenStack\",\n\"deploymentParametersName\":"
 				+ "\"Pass\",\n\"sshKey\":\"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDZVcFCW"
 				+ "LE6uBP0UMpEtTi8TpLJiQZ2UgumitODfsN46WSGnebs481fxGa/YX2nuY5teRIRaqizJOcqoXi9"
@@ -109,9 +118,145 @@ public class DeploymentRestControllerIT {
 				.content(configurationJson)
 				.accept(MediaType.APPLICATION_JSON))
 				.andExpect(jsonPath("name").value("Pass"))
-				.andReturn();*/
+				.andReturn();
 	}
 
+
+	@Test
+	public void cannotDeployOwnAppWithInvalidConfig() throws Exception{
+
+		String token = getToken(testUserName, testPassword);
+		String deploymentJson = "{\"applicationName\":\"Generic server instance\"," +
+				"\"applicationAccountUsername\":\""+TEST_USER_REFERENCE+"\"," +
+				"\"cloudProviderParametersCopy\":null," +
+				"\"attachedVolumes\":[]," +
+				"\"assignedInputs\":[],\"assignedParameters\":[]," +
+				"\"configurationName\":\"config1\"," +
+				"\"configurationAccountUsername\":\""+TEST_USER_REFERENCE+"\"}";
+
+		mockMvc.perform(
+				post("/deployment")
+						.headers(createHeaders(token))
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(deploymentJson)
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNotFound())
+				.andExpect(jsonPath("[0].message").value("Could not find configuration config1 with owner reference '"+TEST_USER_REFERENCE+"'."))
+				.andReturn();
+
+	}
+
+	@Test
+	public void cannotDeployOwnAppWithSharedConfig() throws Exception{
+
+		String token = getToken(testUserName, testPassword);
+		String deploymentJson = "{\"applicationName\":\"Generic server instance\"," +
+				"\"applicationAccountUsername\":\""+TEST_USER_REFERENCE+"\"," +
+				"\"cloudProviderParametersCopy\":null," +
+				"\"attachedVolumes\":[]," +
+				"\"assignedInputs\":[],\"assignedParameters\":[]," +
+				"\"configurationName\":\"config1\"," +
+				"\"configurationAccountUsername\":\""+AJAY_USER_REFERENCE+"\"}";
+
+		MvcResult result = mockMvc.perform(
+				post("/deployment")
+						.headers(createHeaders(token))
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(deploymentJson)
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isForbidden())
+				.andReturn();
+
+		Optional<ConfigurationNotUsableForApplicationException> exception = Optional.ofNullable((ConfigurationNotUsableForApplicationException) result.getResolvedException());
+		exception.ifPresent( (e) -> assertThat(e, is(instanceOf(ConfigurationNotUsableForApplicationException.class))));
+	}
+
+	@Test
+	public void cannotDeployAppWithConfigInDifferentTeam() throws Exception{
+
+		String token = getToken(testUserName, testPassword);
+		String deploymentJson = "{\"applicationName\":\"redis\"," +
+				"\"applicationAccountUsername\":\""+AJAY_USER_REFERENCE+"\"," +
+				"\"cloudProviderParametersCopy\":null," +
+				"\"attachedVolumes\":[]," +
+				"\"assignedInputs\":[],\"assignedParameters\":[]," +
+				"\"configurationName\":\"config1\"," +
+				"\"configurationAccountUsername\":\""+AJAY_USER_REFERENCE+"\"}";
+
+		MvcResult result = mockMvc.perform(
+				post("/deployment")
+						.headers(createHeaders(token))
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(deploymentJson)
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isForbidden())
+				.andReturn();
+
+		Optional<ConfigurationNotUsableForApplicationException> exception = Optional.ofNullable((ConfigurationNotUsableForApplicationException) result.getResolvedException());
+		exception.ifPresent( (e) -> assertThat(e, is(instanceOf(ConfigurationNotUsableForApplicationException.class))));
+	}
+
+	private String getToken(String username, String password) {
+		ResponseEntity<String> response = restTemplate.withBasicAuth(username, password)
+				.getForEntity(aapUrl, String.class);
+		assertThat(response.getStatusCode(), is(equalTo(HttpStatus.OK)));
+		return response.getBody();
+	}
+
+
+	@Test
+	public void cannotDeployOwnAppWithOwnConfigOfSharedCC() throws Exception{
+
+		String token = getToken(testUserName, testPassword);
+		String cloudProviderParametersNameShared = "ostack provider";
+
+		//add deployment parameters
+		String deploymentParametersJson = "{\n\"name\":\"deploy_params\","
+				+ "\n\"fields\":[{\n\"key\":\"floatingip_pool\",\n\"value\":\"net-external\"\n}," +
+				"\n{\n\"key\":\"machine_type\",\n\"value\":\"s1.small\"\n}\n]\n}";
+		mockMvc.perform(
+				post("/configuration/deploymentparameters")
+						.headers(createHeaders(token))
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(deploymentParametersJson)
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("name").value("deploy_params"))
+				.andReturn();
+
+		//add configuration with shared cloud provider parameter
+		String configurationJson = "{\n\"name\":\"config2\",\n\"cloudProviderParametersName\":\""+cloudProviderParametersNameShared+"\"," +
+				"\n\"deploymentParametersName\":\"deploy_params\"," +
+				"\"sshKey\":\"\"}";
+		mockMvc.perform(
+				post("/configuration")
+						.headers(createHeaders(token))
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(configurationJson)
+						.accept(MediaType.APPLICATION_JSON))
+				.andReturn();
+
+
+		String deploymentJson = "{\"applicationName\":\"Generic server instance\"," +
+				"\"applicationAccountUsername\":\""+TEST_USER_REFERENCE+"\"," +
+				"\"cloudProviderParametersCopy\":null," +
+				"\"attachedVolumes\":[]," +
+				"\"assignedInputs\":[],\"assignedParameters\":[]," +
+				"\"configurationName\":\"config2\"," +
+				"\"configurationAccountUsername\":\""+TEST_USER_REFERENCE+"\"}";
+
+		MvcResult result = mockMvc.perform(
+				post("/deployment")
+						.headers(createHeaders(token))
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(deploymentJson)
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isForbidden())
+				.andReturn();
+
+		Optional<ConfigurationNotUsableForApplicationException> exception = Optional.ofNullable((ConfigurationNotUsableForApplicationException) result.getResolvedException());
+		exception.ifPresent( (e) -> assertThat(e, is(instanceOf(ConfigurationNotUsableForApplicationException.class))));
+
+	}
 
 	protected HttpHeaders createHeaders(String token) {
 		return new HttpHeaders() {
