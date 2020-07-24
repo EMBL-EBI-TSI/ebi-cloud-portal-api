7326ff37722602e78f8e60bb57091b326e0e5fc2
YOUR NAME <navis@ebi.ac.uk>
2020-07-24 17:31:53 +0100

    more test cases

--- src/test/java/uk/ac/ebi/tsc/portal/api/configuration/service/ConfigurationServiceUnitTest.java
+++ src/test/java/uk/ac/ebi/tsc/portal/api/configuration/service/ConfigurationServiceUnitTest.java
@@ -15,10 +15,12 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import uk.ac.ebi.tsc.aap.client.repo.DomainService;
 import uk.ac.ebi.tsc.portal.api.account.repo.Account;
+import uk.ac.ebi.tsc.portal.api.application.repo.Application;
 import uk.ac.ebi.tsc.portal.api.cloudproviderparameters.repo.CloudProviderParameters;
 import uk.ac.ebi.tsc.portal.api.cloudproviderparameters.repo.CloudProviderParamsCopy;
 import uk.ac.ebi.tsc.portal.api.cloudproviderparameters.service.CloudProviderParametersNotFoundException;
 import uk.ac.ebi.tsc.portal.api.cloudproviderparameters.service.CloudProviderParametersService;
+import uk.ac.ebi.tsc.portal.api.cloudproviderparameters.service.CloudProviderParamsCopyNotFoundException;
 import uk.ac.ebi.tsc.portal.api.cloudproviderparameters.service.CloudProviderParamsCopyService;
 import uk.ac.ebi.tsc.portal.api.configuration.controller.ConfigurationResource;
 import uk.ac.ebi.tsc.portal.api.configuration.repo.ConfigDeploymentParamsCopy;
@@ -29,9 +31,7 @@ import uk.ac.ebi.tsc.portal.api.deployment.service.DeploymentService;
 import uk.ac.ebi.tsc.portal.api.team.repo.Team;
 import uk.ac.ebi.tsc.portal.api.utils.SendMail;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.Assert.assertTrue;
 import static org.mockito.BDDMockito.given;
@@ -54,6 +54,7 @@ public class ConfigurationServiceUnitTest {
     CloudProviderParameters cpp1;
     String cppReference1 = "myCppReference1";
     String cdpReference1 = "myCdpReference1";
+    String configurationReference1 = "configurationReference1";
     Configuration configuration1;
 
     //second configuration
@@ -65,9 +66,21 @@ public class ConfigurationServiceUnitTest {
     CloudProviderParameters cpp2;
     String cppReference2 = "myCppReference2";
     String cdpReference2 = "myCdpReference2";
+    String configurationReference2 = "configurationReference2";
     Configuration configuration2;
 
-    List configurationList = new ArrayList<Configuration>();
+    //two teams
+    Team team1, team2;
+    String team1Name = "teamName1";
+    String team2Name = "teamName2";
+    String domainReference1 = "domainReference1";
+    String domainReference2 = "domainReference2";
+    Account memberAccount = new Account("memreference", "memusername", "memgivenName", "mempassword", "memmail",
+            new java.sql.Date(2000000000), "memorganisation", "memavatarImageUrl");
+    Set<Team> memberTeams;
+
+    //Application
+    Application application = new Application( "repoUri", "repoPath", "appname", "appreference", account);
 
     @Mock
     ConfigurationRepository configurationRepository;
@@ -105,21 +118,40 @@ public class ConfigurationServiceUnitTest {
         Account account = new Account("reference", "username", "givenName", "password", "email",
                 new java.sql.Date(2000000000), "organisation", "avatarImageUrl");
 
-        //first configuration
+        //memberAccount is member of team1
+        team1 = new Team();
+        team1.setName(team1Name);
+        team1.setDomainReference(domainReference1);
+
+        team1.setAccount(account);
+        team2 = new Team();
+        team2.setName(team2Name);
+        team2.setDomainReference(domainReference2);
+        team2.setAccount(account);
+
+        memberTeams = new HashSet<>();
+        memberTeams.add(team1);
+        memberTeams.add(team2);
+        memberAccount.setMemberOfTeams(memberTeams);
+
+
+        //first configuration, shared with 2 teams
         cdp1 = new ConfigurationDeploymentParameters(cdp1Name, account);
         configDeploymentParamsCopy1 = new ConfigDeploymentParamsCopy(cdp1);
         cppCopy1 = new  CloudProviderParamsCopy(cpp1Name, cloudProvider, account);
         cpp1 = new CloudProviderParameters(cpp1Name, cloudProvider, account);
         configuration1 = new Configuration("myConfig", account,
                 cppCopy1.getName(), cppReference1, "myKey", 2.0, 5.0, configDeploymentParamsCopy1);
+        configuration1.setReference(configurationReference1);
 
-        //second configuration
+        //second configuration, shared with 2 teams
         cdp2 = new ConfigurationDeploymentParameters(cdp2Name, account);
         configDeploymentParamsCopy2 = new ConfigDeploymentParamsCopy(cdp2);
         cppCopy2 = new  CloudProviderParamsCopy(cpp2Name, cloudProvider, account);
         cpp2 = new CloudProviderParameters(cpp2Name, cloudProvider, account);
         configuration2 = new Configuration("myConfig", account,
                 cppCopy2.getName(), cppReference2, "myKey", 2.0, 5.0, configDeploymentParamsCopy2);
+        configuration2.setReference(configurationReference2);
 
 
         cpp1.setReference(cppReference1);
@@ -131,18 +163,12 @@ public class ConfigurationServiceUnitTest {
         configDeploymentParamsCopy1.setConfigurationDeploymentParametersReference(cdpReference1);
         configDeploymentParamsCopy2.setConfigurationDeploymentParametersReference(cdpReference2);
 
-        configurationList = new ArrayList<Configuration>();
-
         // Controller needs RequestContextHolder and HttpServlet Request
         RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(new MockHttpServletRequest()));
     }
 
     @Test
     public void testCheckObsoleteConfigurationsNoneOwnedConfiguration() {
-
-
-        configurationList.add(configuration1);
-        configurationList.add(configuration2);
 
         ConfigurationResource configurationResource1 = new ConfigurationResource(configuration1, cppCopy1);
         ConfigurationResource configurationResource2 = new ConfigurationResource(configuration2, cppCopy2);
@@ -171,6 +197,7 @@ public class ConfigurationServiceUnitTest {
     public void testCheckObsoleteConfigurationsOwnedConfigurationCPPNotFound() {
 
 
+        List configurationList = new ArrayList<Configuration>();
         configurationList.add(configuration1);
         configurationList.add(configuration2);
 
@@ -201,7 +228,7 @@ public class ConfigurationServiceUnitTest {
     @Test
     public void testCheckObsoleteConfigurationsOwnedConfigurationCDPNotFound() {
 
-
+        List configurationList = new ArrayList<Configuration>();
         configurationList.add(configuration1);
         configurationList.add(configuration2);
 
@@ -226,6 +253,233 @@ public class ConfigurationServiceUnitTest {
 
         assertTrue(obsoleteConfigurationList .get(0).isObsolete() == true);
         assertTrue(obsoleteConfigurationList .get(1).isObsolete() == false);
+
+    }
+
+    @Test
+    public void testCheckObsoleteConfigurationsNoneSharedConfigurationCPPNotShared() {
+
+        Set<Team> configurationSharedWithTeams =  new HashSet<>();
+        configurationSharedWithTeams.add(team1);
+        configuration1.setSharedWithTeams(configurationSharedWithTeams);
+
+        List configurationList = new ArrayList<Configuration>();
+        configurationList.add(configuration1);
+        configurationList.add(configuration2);
+
+        ConfigurationResource configurationResource1 = new ConfigurationResource(configuration1, cppCopy1);
+        ConfigurationResource configurationResource2 = new ConfigurationResource(configuration2, cppCopy2);
+        configurationResource1.setConfigDeploymentParametersReference(cdpReference1);
+        configurationResource2.setConfigDeploymentParametersReference(cdpReference2);
+        configurationResource1.setReference(configurationReference1);
+        configurationResource2.setReference(configurationReference2);
+
+        configResourceList.add(configurationResource1);
+        configResourceList.add(configurationResource2);
+
+        given(cppCopyService.findByCloudProviderParametersReference(cppReference1)).willReturn(cppCopy1);
+        given(cppCopyService.findByCloudProviderParametersReference(cppReference2)).willReturn(cppCopy2);
+        given(cppService.findByReference(cppReference1)).willReturn(cpp1);
+        given(cppService.findByReference(cppReference2)).willReturn(cpp2);
+        given(cdpCopyService.findByConfigurationDeploymentParametersReference(cdpReference1)).willReturn(configDeploymentParamsCopy1);
+        given(cdpCopyService.findByConfigurationDeploymentParametersReference(cdpReference2)).willReturn(configDeploymentParamsCopy2);
+        given(cdpService.findByReference(configDeploymentParamsCopy1.getConfigurationDeploymentParametersReference())).willReturn(cdp1);
+        given(cdpService.findByReference(configDeploymentParamsCopy2.getConfigurationDeploymentParametersReference())).willReturn(cdp2);
+
+        given(configurationRepository.findByReference(configurationReference1)).willReturn(Optional.of(configuration1));
+        given(configurationRepository.findByReference(configurationReference2)).willReturn(Optional.of(configuration2));
+        List<ConfigurationResource> obsoleteConfigurationList = testCandidate.checkObsoleteConfigurations(configResourceList,
+                memberAccount, cdpCopyService);
+
+        assertTrue(obsoleteConfigurationList .get(0).isObsolete() == true);
+        assertTrue(obsoleteConfigurationList .get(1).isObsolete() == true);
+
+    }
+
+    @Test
+    public void testCheckObsoleteConfigurationsNoneSharedConfigurationCPPSharedWithTeam1() {
+
+        Set<Team> cppSharedTeams = new HashSet<>();
+        cppSharedTeams.add(team1);
+        cpp1.setSharedWithTeams(cppSharedTeams);
+
+        Set<Team> cdpSharedTeams = new HashSet<>();
+        cdpSharedTeams.add(team1);
+        cdp1.setSharedWithTeams(cdpSharedTeams);
+
+        Set<Team> configurationSharedWithTeams =  new HashSet<>();
+        configurationSharedWithTeams.add(team1);
+        configuration1.setSharedWithTeams(configurationSharedWithTeams);
+
+        List configurationList = new ArrayList<Configuration>();
+        configurationList.add(configuration1);
+        configurationList.add(configuration2);
+
+        ConfigurationResource configurationResource1 = new ConfigurationResource(configuration1, cppCopy1);
+        ConfigurationResource configurationResource2 = new ConfigurationResource(configuration2, cppCopy2);
+        configurationResource1.setConfigDeploymentParametersReference(cdpReference1);
+        configurationResource2.setConfigDeploymentParametersReference(cdpReference2);
+        configurationResource1.setReference(configurationReference1);
+        configurationResource2.setReference(configurationReference2);
+
+        configResourceList.add(configurationResource1);
+        configResourceList.add(configurationResource2);
+
+        given(cppCopyService.findByCloudProviderParametersReference(cppReference1)).willReturn(cppCopy1);
+        given(cppCopyService.findByCloudProviderParametersReference(cppReference2)).willReturn(cppCopy2);
+        given(cppService.findByReference(cppReference1)).willReturn(cpp1);
+        given(cppService.findByReference(cppReference2)).willReturn(cpp2);
+        given(cdpCopyService.findByConfigurationDeploymentParametersReference(cdpReference1)).willReturn(configDeploymentParamsCopy1);
+        given(cdpCopyService.findByConfigurationDeploymentParametersReference(cdpReference2)).willReturn(configDeploymentParamsCopy2);
+        given(cdpService.findByReference(configDeploymentParamsCopy1.getConfigurationDeploymentParametersReference())).willReturn(cdp1);
+        given(cdpService.findByReference(configDeploymentParamsCopy2.getConfigurationDeploymentParametersReference())).willReturn(cdp2);
+
+        given(configurationRepository.findByReference(configurationReference1)).willReturn(Optional.of(configuration1));
+        given(configurationRepository.findByReference(configurationReference2)).willReturn(Optional.of(configuration2));
+        List<ConfigurationResource> obsoleteConfigurationList = testCandidate.checkObsoleteConfigurations(configResourceList,
+                memberAccount, cdpCopyService);
+
+        assertTrue(obsoleteConfigurationList .get(0).isObsolete() == false);
+        assertTrue(obsoleteConfigurationList .get(1).isObsolete() == true);
+
+    }
+
+    @Test
+    public void testCheckObsoleteConfigurationsNoneSharedConfigurationCPPAndCDPSharedWithTeam1() {
+
+        Set<Team> cppSharedTeams = new HashSet<>();
+        cppSharedTeams.add(team1);
+        cpp1.setSharedWithTeams(cppSharedTeams);
+
+        Set<Team> cdpSharedTeams = new HashSet<>();
+        cdpSharedTeams.add(team1);
+        cdp1.setSharedWithTeams(cdpSharedTeams);
+
+        Set<Team> configurationSharedWithTeams =  new HashSet<>();
+        configurationSharedWithTeams.add(team1);
+        configuration1.setSharedWithTeams(configurationSharedWithTeams);
+
+        List configurationList = new ArrayList<Configuration>();
+        configurationList.add(configuration1);
+        configurationList.add(configuration2);
+
+        ConfigurationResource configurationResource1 = new ConfigurationResource(configuration1, cppCopy1);
+        ConfigurationResource configurationResource2 = new ConfigurationResource(configuration2, cppCopy2);
+        configurationResource1.setConfigDeploymentParametersReference(cdpReference1);
+        configurationResource2.setConfigDeploymentParametersReference(cdpReference2);
+        configurationResource1.setReference(configurationReference1);
+        configurationResource2.setReference(configurationReference2);
+
+        configResourceList.add(configurationResource1);
+        configResourceList.add(configurationResource2);
+
+        given(cppCopyService.findByCloudProviderParametersReference(cppReference1)).willReturn(cppCopy1);
+        given(cppCopyService.findByCloudProviderParametersReference(cppReference2)).willReturn(cppCopy2);
+        given(cppService.findByReference(cppReference1)).willReturn(cpp1);
+        given(cppService.findByReference(cppReference2)).willReturn(cpp2);
+        given(cdpCopyService.findByConfigurationDeploymentParametersReference(cdpReference1)).willReturn(configDeploymentParamsCopy1);
+        given(cdpCopyService.findByConfigurationDeploymentParametersReference(cdpReference2)).willReturn(configDeploymentParamsCopy2);
+        given(cdpService.findByReference(configDeploymentParamsCopy1.getConfigurationDeploymentParametersReference())).willReturn(cdp1);
+        given(cdpService.findByReference(configDeploymentParamsCopy2.getConfigurationDeploymentParametersReference())).willReturn(cdp2);
+
+        given(configurationRepository.findByReference(configurationReference1)).willReturn(Optional.of(configuration1));
+        given(configurationRepository.findByReference(configurationReference2)).willReturn(Optional.of(configuration2));
+        List<ConfigurationResource> obsoleteConfigurationList = testCandidate.checkObsoleteConfigurations(configResourceList,
+                memberAccount, cdpCopyService);
+
+        assertTrue(obsoleteConfigurationList .get(0).isObsolete() == false);
+        assertTrue(obsoleteConfigurationList .get(1).isObsolete() == true);
+
+    }
+
+    @Test
+    public void testCanConfigurationBeUsedForApplicationOwnConfigurationAndApplicationValidCPP(){
+
+        given(cppService.findByReference(cppReference1)).willReturn(cpp1);
+        given(cppService.canCredentialBeUsedForApplication(cpp1, application, account)).willCallRealMethod();
+        boolean canConfigurationBeUsedForApplication = testCandidate.canConfigurationBeUsedForApplication(configuration1, application, account);
+        assertTrue(canConfigurationBeUsedForApplication == true);
+
+    }
+
+    @Test
+    public void testCanConfigurationBeUsedForApplicationOwnConfigurationAndApplicationInValidCPP(){
+
+        given(cppService.findByReference(cppReference1)).willThrow(CloudProviderParametersNotFoundException.class);
+        boolean canConfigurationBeUsedForApplication = testCandidate.canConfigurationBeUsedForApplication(configuration1, application, account);
+        assertTrue(canConfigurationBeUsedForApplication == false);
+
+    }
+
+    @Test
+    public void testCanConfigurationBeUsedForApplicationSharedConfigurationAndApplicationValidCPP(){
+
+        Set<Team> cppSharedTeams = new HashSet<>();
+        cppSharedTeams.add(team1);
+        cpp1.setSharedWithTeams(cppSharedTeams);
+
+        Set<Team> appSharedTeams = new HashSet<>();
+        appSharedTeams.add(team1);
+        application.setSharedWithTeams(appSharedTeams);
+
+        Set<Team> configurationSharedWithTeams =  new HashSet<>();
+        configurationSharedWithTeams.add(team1);
+        configuration1.setSharedWithTeams(configurationSharedWithTeams);
+
+        given(cppService.findByReference(cppReference1)).willReturn(cpp1);
+        given(cppService.checkForOverlapingAmongTeams(Mockito.anySet(),Mockito.anySet(), Mockito.anySet())).willCallRealMethod();
+        boolean canConfigurationBeUsedForApplication = testCandidate.canConfigurationBeUsedForApplication(configuration1, application, memberAccount);
+        assertTrue(canConfigurationBeUsedForApplication == true);
+
+    }
+
+    @Test
+    public void testCanConfigurationBeUsedForApplicationSharedConfigurationAndApplicationDifferentTeams(){
+
+        Set<Team> cppSharedTeams = new HashSet<>();
+        cppSharedTeams.add(team1);
+        cpp1.setSharedWithTeams(cppSharedTeams);
+
+        Set<Team> appSharedTeams = new HashSet<>();
+        appSharedTeams.add(team2);
+        application.setSharedWithTeams(appSharedTeams);
+
+        Set<Team> configurationSharedWithTeams =  new HashSet<>();
+        configurationSharedWithTeams.add(team1);
+        configuration1.setSharedWithTeams(configurationSharedWithTeams);
+
+        given(cppService.findByReference(cppReference1)).willReturn(cpp1);
+        given(cppService.checkForOverlapingAmongTeams(Mockito.anySet(),Mockito.anySet(), Mockito.anySet())).willCallRealMethod();
+        boolean canConfigurationBeUsedForApplication = testCandidate.canConfigurationBeUsedForApplication(configuration1, application, memberAccount);
+        assertTrue(canConfigurationBeUsedForApplication == false);
+
+    }
+
+    @Test
+    public void testCreateConfigurationResource(){
+
+        String cppCopyNotFoundName = "cppCopyNotFoundName";
+        String cppCopyNotFoundReference = "cppCopyNotFoundReference";
+
+        ConfigurationDeploymentParameters cdp3 = new ConfigurationDeploymentParameters("cdp3Name", account);
+        ConfigDeploymentParamsCopy notFoundCopy = new ConfigDeploymentParamsCopy(cdp3);
+        List configurationList = new ArrayList<Configuration>();
+        configurationList.add(configuration1);
+        configurationList.add(configuration2);
+
+
+        Configuration oneWithoutCPPCopy = new Configuration("myConfig", account,
+                cppCopyNotFoundName, cppCopyNotFoundReference, "myKey", 2.0, 5.0, notFoundCopy);
+
+
+        configurationList.add(oneWithoutCPPCopy);
+
+        given(cppCopyService.findByCloudProviderParametersReference(cppReference1)).willReturn(cppCopy1);
+        given(cppCopyService.findByCloudProviderParametersReference(cppReference2)).willReturn(cppCopy2);
+        given(cppCopyService.findByCloudProviderParametersReference(cppCopyNotFoundReference)).willThrow(CloudProviderParamsCopyNotFoundException.class);
+
+        List<ConfigurationResource> resourceList = testCandidate.createConfigurationResource(configurationList);
+        assertTrue(resourceList.size() == 3);
 
     }
 
