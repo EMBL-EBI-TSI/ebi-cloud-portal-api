a8825ad7e06cd5fbfe8f14435c82db742b09754f
Navis <navis@ebi.ac.uk>
2021-03-22 11:57:46 +0000

    wiremock IT tests

--- src/integration-test/java/uk/ac/ebi/tsi/portal/api/team/TeamRestControllerIT.java
+++ src/integration-test/java/uk/ac/ebi/tsi/portal/api/team/TeamRestControllerIT.java
@@ -1,19 +1,22 @@
 package uk.ac.ebi.tsi.portal.api.team;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import org.apache.log4j.Logger;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -22,104 +25,104 @@ import org.springframework.web.util.NestedServletException;
 import uk.ac.ebi.tsc.portal.BePortalApiApplication;
 import uk.ac.ebi.tsc.portal.api.team.service.TeamAccessDeniedException;
 import uk.ac.ebi.tsc.portal.config.WebConfiguration;
+import uk.ac.ebi.tsc.portal.security.EcpAuthenticationService;
 
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ContextConfiguration(classes = {WebConfiguration.class, BePortalApiApplication.class})
-@TestPropertySource("classpath:integrationTest.properties")
+@TestPropertySource(value = "classpath:integrationTest.properties", properties = {"aap.domains.url = http://localhost:9000"})
 @AutoConfigureMockMvc
 public class TeamRestControllerIT {
 	
-	 private static final Logger logger = Logger.getLogger(TeamRestControllerIT.class);
+	private static final Logger logger = Logger.getLogger(TeamRestControllerIT.class);
 
-		@Autowired
-	    private TestRestTemplate restTemplate;
-	    
-		@Autowired
-		private MockMvc mockMvc;
-		
-		@Autowired 
-		private ObjectMapper mapper;
-		
-		@Value("${aapUserName}")
-		private String aapUserName;
-		
-		@Value("${aapPassword}")
-		private String aapPassword;
-		
-		@Value("${aapUrl}")
-		private String aapUrl;
-		
-		private String token;
-		
-		String teamName = "SOME_TEAM_NAME";
+	@Autowired
+	private TestRestTemplate restTemplate;
 
-	@Value("${ajayUserName}")
-	private String ajayUserName;
+	@Autowired
+	private MockMvc mockMvc;
 
-	@Value("${ajayPassword}")
-	private String ajayPassword;
-		
-		@Before
-		public void setup() throws Exception{
-			//get jwt token
-			ResponseEntity<String> response = restTemplate.withBasicAuth(aapUserName, aapPassword)
-					.getForEntity(aapUrl, String.class);
-			token = response.getBody();
-		}
+	@Autowired
+	private ObjectMapper mapper;
 
-		@Test
-		public void can_get_a_team() throws Exception{
-			MockHttpServletResponse response = mockMvc.perform(
-					get("/team")
-					.headers(createHeaders(token))
-					.contentType(MediaType.APPLICATION_JSON)
-					.accept(MediaType.APPLICATION_JSON)
-					)
-					.andExpect(status().isOk())
-					.andReturn().getResponse();
+	@Value("${aapUserName}")
+	private String aapUserName;
 
-			logger.info("Response "  +response);
-		}
+	@Value("${aapPassword}")
+	private String aapPassword;
+
+	@Value("${aapUrl}")
+	private String aapUrl;
+
+	@MockBean
+	private EcpAuthenticationService authenticationService;
+
+	@Rule
+	public WireMockRule mockDomainService = new WireMockRule(wireMockConfig().port(9000));
+
+	@Before
+	public void setup() {
+		when(authenticationService.getAuthentication(any())).thenReturn(SecurityContextHolder.getContext().getAuthentication());
+	}
 
 	@Test
+	@WithMockUser(username = "usr-e8c1d6d5-6bf4-4636-a70e-41b8f32c70b4")
+	public void can_get_a_team() throws Exception {
+		MockHttpServletResponse response = mockMvc.perform(
+				get("/team")
+						.header("Authorization", "Bearer sometoken")
+						.contentType(MediaType.APPLICATION_JSON)
+						.accept(MediaType.APPLICATION_JSON)
+		)
+				.andExpect(status().isOk())
+				.andReturn().getResponse();
+
+		logger.info("Response " + response);
+	}
+
+	@Test
+	@WithMockUser(username = "usr-e8c1d6d5-6bf4-4636-a70e-41b8f32c70b4")
 	public void team_owner_can_add_team_contact_emails() throws Exception {
 
 		String emails = "contact1@ebi,contact2@ebi";
 		mockMvc.perform(
 				put("/team/" + "test-team1" + "/contactemail/")
-						.headers(createHeaders(getToken(ajayUserName, ajayPassword)))
+						.header("Authorization", "Bearer sometoken")
 						.contentType(MediaType.APPLICATION_JSON)
 						.content(emails)
 						.accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk());
-
 	}
 
 	@Test
+	@WithMockUser(username = "usr-e8c1d6d5-6bf4-4636-a70e-41b8f32c70b4")
 	public void team_owner_can_remove_team_contact_emails() throws Exception {
 
 		String deleteURL = "/team/" + "test-team1" + "/contactemail/" + "contact@ebi";
 		mockMvc.perform(
 				delete(deleteURL)
-						.headers(createHeaders(getToken(ajayUserName, ajayPassword)))
+						.header("Authorization", "Bearer sometoken")
 						.contentType(MediaType.APPLICATION_JSON)
 						.accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isNoContent());
 	}
 
 	@Test
+	@WithMockUser(username = "usr-b070585b-a340-4a98-aff1-f3de48da8c38")
 	public void non_team_owner_cannot_add_team_contact_emails() throws Exception {
 
 		String emails = "contact1@ebi,contact2@ebi";
 		try {
 			mockMvc.perform(
 					put("/team/" + "test-team1" + "/contactemail/")
-							.headers(createHeaders(token))
+							.header("Authorization", "Bearer sometoken")
 							.contentType(MediaType.APPLICATION_JSON)
 							.content(emails)
 							.accept(MediaType.APPLICATION_JSON))
@@ -130,13 +133,14 @@ public class TeamRestControllerIT {
 	}
 
 	@Test
+	@WithMockUser(username = "usr-b070585b-a340-4a98-aff1-f3de48da8c38")
 	public void non_team_owner_cannot_remove_team_contact_emails() throws Exception {
 
 		String deleteURL = "/team/" + "test-team1" + "/contactemail/" + "contact@ebi";
 		try {
 			mockMvc.perform(
 					post(deleteURL)
-							.headers(createHeaders(token))
+							.header("Authorization", "Bearer sometoken")
 							.contentType(MediaType.APPLICATION_JSON)
 							.accept(MediaType.APPLICATION_JSON))
 			;
@@ -146,24 +150,16 @@ public class TeamRestControllerIT {
 	}
 
 	@Test
+	@WithMockUser(username = "usr-b070585b-a340-4a98-aff1-f3de48da8c38")
 	public void getAllTeamsForCurrentUser() throws Exception {
 		mockMvc.perform(
 				get("/team/all")
-						.headers(createHeaders(token))
+						.header("Authorization", "Bearer sometoken")
 						.contentType(MediaType.APPLICATION_JSON)
 						.accept(MediaType.APPLICATION_JSON)
 		)
 				.andExpect(status().isOk());
 	}
-
-	private String getToken(String username, String password) {
-		ResponseEntity<String> response = restTemplate.withBasicAuth(username, password)
-				.getForEntity(aapUrl, String.class);
-		return response.getBody();
-	}
-
-
-		
 		/*@Test
 		public void add_member_to_team() throws Exception{
 			
@@ -193,19 +189,5 @@ public class TeamRestControllerIT {
 			.andExpect(status().isOk());
 
 		}*/
-
-
-
-		
-		protected HttpHeaders createHeaders(String token) {
-			return new HttpHeaders() {
-				private static final long serialVersionUID = 1L;
-			{
-				String authHeader = "Bearer " + token;
-				set("Authorization", authHeader);
-				set("Content-Type", "application/json");
-				set("Accept", "application/hal+json");
-			}};
-		}
 
 }
